a58657e180ea417b398b1792c1e6558f50375688
Pallavi Pai <pai.pallavi3110@gmail.com>
2026-06-14 18:43:23 +0530

    Revert "feat: storefront shows products by default, sign-in button for guests"
    This reverts commit ae4d67812b2f96d91b7c8f549c49e6f7e4217278.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -132,8 +132,8 @@ function App() {
         </div>
       ) : (
         <>
-          {!isAuthenticated && currentPage === 'login' ? (
-            <Landing onLoginSuccess={() => setCurrentPage('products')} />
+          {!isAuthenticated ? (
+            <Landing onLoginSuccess={() => {}} />
           ) : (
             <>
               <Navbar 
@@ -143,7 +143,6 @@ function App() {
                 apiStatus={apiStatus}
                 user={user}
                 onLogout={handleLogout}
-                isAuthenticated={isAuthenticated}
               />
 
               <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
